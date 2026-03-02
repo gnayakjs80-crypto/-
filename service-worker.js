1092931b7c92c06e1ec346be9da344e2fcbb3ff9
gnayakjs80-crypto <gnayakjs80@gmail.com>
2026-03-02 18:45:02 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE = 'jiri-v1';
+const FILES = [
+  './geography-flow2.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
